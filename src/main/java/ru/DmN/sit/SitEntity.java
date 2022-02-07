67c4b539706d3a175d4e83f70f4a46b4f73b25e0
Domaman202 <vip.domaman@mail.ru>
2022-02-07 11:35:32 +0700

    Revert "x"
    This reverts commit 1ec7d7c5ad3b14b4300652656beac8177b6b6f6b.

--- src/main/java/ru/DmN/sit/SitEntity.java
+++ src/main/java/ru/DmN/sit/SitEntity.java
@@ -7,8 +7,6 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import java.util.concurrent.CompletableFuture;
-
 public class SitEntity extends ArmorStandEntity {
     public Vec3d old;
 
@@ -25,26 +23,15 @@ public class SitEntity extends ArmorStandEntity {
 
     @Override
     protected void removePassenger(Entity passenger) {
+        passenger.setPosition(old);
         super.removePassenger(passenger);
-        CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if (passenger.getVehicle() == null) {
-//                System.out.println(passenger.getVehicle() == this);
-                System.out.println("NIGGER KILLED!");
-                passenger.setPosition(old);
-                this.kill();
-            }
-        });
+        this.kill();
     }
 
     @Override
-    public boolean saveNbt(NbtCompound nbt) {
-        System.out.println("KILLED!");
+    public void removeAllPassengers() {
+        this.getPassengerList().forEach((passenger) -> passenger.setPosition(old));
+        super.removeAllPassengers();
         this.kill();
-        return false;
     }
 }
