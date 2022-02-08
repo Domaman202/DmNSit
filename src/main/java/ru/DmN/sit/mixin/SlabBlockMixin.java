14a3b51be8cf733cddf20b55c718d0be533a68e6
Domaman202 <vip.domaman@mail.ru>
2022-02-08 13:36:41 +0700

    update

--- src/main/java/ru/DmN/sit/mixin/SlabBlockMixin.java
+++ src/main/java/ru/DmN/sit/mixin/SlabBlockMixin.java
@@ -3,28 +3,34 @@ package ru.DmN.sit.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.block.enums.SlabType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import ru.DmN.sit.Main;
 
 @Mixin(SlabBlock.class)
 public class SlabBlockMixin extends Block {
+    @Shadow @Final public static EnumProperty<SlabType> TYPE;
+
     public SlabBlockMixin(Settings settings) {
         super(settings);
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (world.isClient)
-            return ActionResult.CONSUME;
+        if (world.isClient || state.get(TYPE) != SlabType.BOTTOM)
+            return ActionResult.PASS;
         Main.sit((ServerPlayerEntity) player, (ServerWorld) world, new Vec3d(pos.getX(), pos.getY(), pos.getZ()), player.getPos());
         return ActionResult.SUCCESS;
     }
--- src/main/java/ru/DmN/sit/mixin/StairsBlockMixin.java
+++ src/main/java/ru/DmN/sit/mixin/StairsBlockMixin.java
@@ -4,28 +4,34 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import ru.DmN.sit.Main;
 
 @Mixin(StairsBlock.class)
 public class StairsBlockMixin extends Block {
+    @Shadow @Final public static EnumProperty<BlockHalf> HALF;
+
     public StairsBlockMixin(AbstractBlock.Settings settings) {
         super(settings);
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (world.isClient)
-            return ActionResult.CONSUME;
+        if (world.isClient || state.get(HALF) != BlockHalf.BOTTOM)
+            return ActionResult.PASS;
         Main.sit((ServerPlayerEntity) player, (ServerWorld) world, new Vec3d(pos.getX(), pos.getY(), pos.getZ()), player.getPos());
         return ActionResult.SUCCESS;
     }
