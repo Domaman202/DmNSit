299fe3d6cd797c4348b74ed45c4a0e030d2f813e
Domaman202 <vip.domaman@mail.ru>
2022-03-04 18:04:51 +0700

    ;3

--- src/main/java/ru/DmN/sit/Main.java
+++ src/main/java/ru/DmN/sit/Main.java
@@ -1,24 +1,40 @@
 package ru.DmN.sit;
 
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class Main implements ModInitializer {
+    public static final Map<ServerPlayerEntity, Boolean> autoSit = new HashMap<>();
+
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(literal("sit").executes(context -> {
-            var pos = context.getSource().getPosition();
-            sit(context.getSource().getPlayer(), context.getSource().getWorld(), pos, pos);
-            return 1;
-        })));
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            dispatcher.register(literal("sit").executes(context -> {
+                var pos = context.getSource().getPosition();
+                sit(context.getSource().getPlayer(), context.getSource().getWorld(), pos, pos);
+                return 1;
+            }));
+
+            dispatcher.register(literal("asit").then(argument("active", BoolArgumentType.bool()).executes(context -> {
+                autoSit.put(context.getSource().getPlayer(), context.getArgument("active", boolean.class));
+                return 1;
+            })));
+        });
     }
 
     public static void sit(ServerPlayerEntity player, ServerWorld world, Vec3d pos, Vec3d old) {
